263bff00ec13cd3611332116bd693add15f4b626
AnshulPandey-01 <anshulpandey.indore@gmail.com>
2021-06-17 17:52:00 +0530

    dialect fixes

--- src/main/java/com/anshul/examportal/admin/SampleDownloadFiles.java
+++ src/main/java/com/anshul/examportal/admin/SampleDownloadFiles.java
@@ -25,14 +25,6 @@ public class SampleDownloadFiles {
 		String currentDirectory = System.getProperty("user.dir");
 		File file = new File(currentDirectory + "/Excel_Sample_File/" + fileName + ".xlsx");
 		
-		File directoryPath = new File(currentDirectory);
-		String contents[] = directoryPath.list();
-		
-		System.out.println("List of files and directories in the specified directory:");
-		for(int i=0; i<contents.length; i++) {
-			System.out.println(contents[i]);
-		}
-		
 		Path path = Paths.get(file.getAbsolutePath());
 		ByteArrayResource resource = null;
 		try {
