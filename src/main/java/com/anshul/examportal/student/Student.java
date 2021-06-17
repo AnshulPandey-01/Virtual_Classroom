456a000e6227e3259b378f65828dba5eb8bf33a1
AnshulPandey-01 <anshulpandey.indore@gmail.com>
2021-06-17 20:58:11 +0530

    bug fixes

--- src/main/java/com/anshul/examportal/student/Student.java
+++ src/main/java/com/anshul/examportal/student/Student.java
@@ -3,7 +3,6 @@ package com.anshul.examportal.student;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Student {
