b714f117f5f045e05653d780a7c3aa8184e7cff0
AnshulPandey-01 <anshulpandey.indore@gmail.com>
2021-07-29 01:35:00 +0530

    clean up

--- src/main/java/com/anshul/examportal/faculty/Faculty.java
+++ src/main/java/com/anshul/examportal/faculty/Faculty.java
@@ -3,7 +3,6 @@ package com.anshul.examportal.faculty;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Faculty {
