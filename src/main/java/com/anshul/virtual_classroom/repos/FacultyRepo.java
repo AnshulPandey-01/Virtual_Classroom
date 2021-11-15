4f2e5ce044fba9d1ace88c305f895bd30f8dfb26
AnshulPandey-01 <anshulpandey.indore@gmail.com>
2021-11-16 01:22:17 +0530

    fixes

--- src/main/java/com/anshul/virtual_classroom/repos/FacultyRepo.java
+++ src/main/java/com/anshul/virtual_classroom/repos/FacultyRepo.java
@@ -1,5 +1,7 @@
 package com.anshul.virtual_classroom.repos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,8 @@ import com.anshul.virtual_classroom.entity.Faculty;
 
 @Repository
 public interface FacultyRepo extends JpaRepository<Faculty, String> {
+	
+	Optional<Faculty> findByName(String name);
 	
 	@Query(value = "select count(*) from faculty where name= :name or email= :email", nativeQuery = true)
 	int checkFacultyExists(@Param("name") String name, @Param("email") String email);
